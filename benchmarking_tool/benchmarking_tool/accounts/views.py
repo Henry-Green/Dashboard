1438b406cef9202f82461a3e9ab058db0c61f835
henrygreen12 <76181394+henrygreen12@users.noreply.github.com>
2021-08-18 14:28:48 -0400

    Update views.py

--- benchmarking_tool/benchmarking_tool/accounts/views.py
+++ benchmarking_tool/benchmarking_tool/accounts/views.py
@@ -178,7 +178,7 @@ def login():
         form = LoginForm(email="")
     return redirect(url_for('accounts.commerciallogin'))	
     return render_template('login.html', title='Login', form=form,last_updated=dir_last_updated())
-
+@commercial.route('/')
 @accounts.route('/commerciallogin', methods=['GET', 'POST'])
 def commerciallogin():
     hashed_password = bcrypt.generate_password_hash('password').decode('utf-8')
